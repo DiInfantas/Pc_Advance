a1c8455d854c858b4e28640b481413142a8079c8
DiInfantas <127537754+DiInfantas@users.noreply.github.com>
2023-06-27 19:00:31 -0400

    1.2

--- core/models.py
+++ core/models.py
@@ -2,3 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Categoria(models.Model):
+    idCategoria = models.AutoField(primary_key=True)
+    NombreCategoria =models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.NombreCategoria
+
+class Producto(models.Model):
+    idProducto= models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=100)
+    precio=models.CharField(max_length=100)
+    modelo=models.CharField(max_length=100)
+    categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+    
+
+    
